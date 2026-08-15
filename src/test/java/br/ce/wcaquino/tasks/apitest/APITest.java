fdb38f441b2323f6472ba3db55ccf238bd13883f
Pedro <70184804+luizsfl@users.noreply.github.com>
2026-08-15 20:20:42 -0300

    Update APITest.java

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -23,18 +23,7 @@ public class APITest {
 			.statusCode(200)
 		;
 	}
-	
-	@Test
-	public void deveAdicionarTarefaComSucesso() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201)
-		;
-	}
+
 	
 	@Test
 	public void naoDeveAdicionarTarefaInvalida() {
@@ -49,28 +38,4 @@ public class APITest {
 		;
 	}
 	
-	@Test
-	public void deveRemoverTarefaComSucesso() {
-		//inserir
-		Integer id = RestAssured.given()
-			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-//			.log().all()
-			.statusCode(201)
-			.extract().path("id")
-		;
-		
-		System.out.println(id);
-		
-		//remover
-		RestAssured.given()
-		.when()
-			.delete("/todo/"+id)
-		.then()
-			.statusCode(204)
-		;
-	}
 }
